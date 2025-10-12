83b5c658102cec37418419817040a95f97d64cfb
Justin Sniesak <justindsniesak@gmail.com>
2025-10-11 23:01:49 -0700

    Clean up spelling.

--- gcpcrcb.go
+++ gcpcrcb.go
@@ -10,7 +10,7 @@ func greetingTimeDate() {
 	currentTime := time.Now()
 	dateFormat := currentTime.Format("01/02/2006")
 	timeFormat := currentTime.Format("03:04 PM")
-	fmt.Println(greeting + ", the date is " + dateFormat + ", and the time is " + timeFormat + ".")
+	fmt.Println(greeting + "the date is " + dateFormat + ", and the time is " + timeFormat + ".")
 }
 
 func main() {
