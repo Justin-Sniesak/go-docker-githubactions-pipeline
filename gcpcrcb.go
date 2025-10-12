6c6f82116b349afe87dff57f6897f25a0f7abb49
Justin Sniesak <justindsniesak@gmail.com>
2025-10-11 22:55:55 -0700

    Change name to greeting and update code

--- gcpcrcb.go
+++ gcpcrcb.go
@@ -6,11 +6,11 @@ import (
 )
 
 func greetingTimeDate() {
-	name := "Justin"
+	greeting := "Hello, "
 	currentTime := time.Now()
 	dateFormat := currentTime.Format("01/02/2006")
 	timeFormat := currentTime.Format("03:04 PM")
-	fmt.Println(name + ", the date is " + dateFormat + ", and the time is " + timeFormat + ".")
+	fmt.Println(greeting + ", the date is " + dateFormat + ", and the time is " + timeFormat + ".")
 }
 
 func main() {
